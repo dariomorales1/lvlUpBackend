449f17780d5f75b90e603a3c2b48dc1a86df6daf
dariomorales1 <dar.moralesa@duocuc.cl>
2025-11-08 10:44:36 -0300

    Creacion de los microservicios

---
+++ src/main/java/cl/levelup/levelupbackend/LevelUpBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.levelup.levelupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelUpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LevelUpBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/levelup/levelupbackend/LevelUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.levelup.levelupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelUpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
